fbe24bba4e770af00b3540332f02256b0b83be19
Amy <amysaz@gmail.com>
2019-11-02 21:02:53 -0500

    GameMode Updates

--- gameModes.py
+++ gameModes.py
@@ -38,3 +38,17 @@ class QuestionMode(mode, canvas):
     def redrawAll(mode, canvas):
         font = 'Arial 26 bold'
         canvas.create_text(mode.width//2,mode.height//2,text="Question",font=font)
+
+###############################
+# Making app
+###############################
+
+class OurApp(ModalApp):
+    def appStarted(app):
+        app.splashScreenMode = SplashScreenMode()
+        app.gameMode = GameMode()
+        app.helpMode = HelpMode()
+        app.questionMode = QuestionMode()
+        app.setActiveMode(app.splashScreenMode)
+        
+app = OurApp(width = 500, height = 500)
